e2734f25eb0fdd015cc38c918c7e59e5a9a3f879
Peter Morlion <peter.morlion@gmail.com>
2017-10-11 10:13:49 +0200

    Change docker to docker-for-windows

--- script.ps1
+++ script.ps1
@@ -46,4 +46,4 @@ choco install -y winmerge
 choco install -y bfg-repo-cleaner
 choco install -y logparserstudio
 choco install -y spotify
-choco install -y docker
+choco install -y docker-for-windows
