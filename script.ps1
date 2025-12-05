9c61f4c0d33659fb79453da49727e0e7be56b68e
Peter Morlion <peter.morlion@gmail.com>
2025-12-05 14:19:21 +0100

    Use latest version of SSMS

--- script.ps1
+++ script.ps1
@@ -2,7 +2,7 @@ winget install -e --id FileZilla.FileZilla --accept-source-agreements --accept-p
 winget install -e --id GIMP.GIMP --accept-source-agreements --accept-package-agreements
 winget install -e --id Git.Git --accept-source-agreements --accept-package-agreements
 winget install -e --id Microsoft.VisualStudioCode --accept-source-agreements --accept-package-agreements
-winget install -e --id Microsoft.SQLServerManagementStudio --accept-source-agreements --accept-package-agreements
+winget install -e --id Microsoft.SQLServerManagementStudio.21 --accept-source-agreements --accept-package-agreements
 winget install -e --id SlackTechnologies.Slack --accept-source-agreements --accept-package-agreements
 winget install -e --id dotPDNLLC.paintdotnet --accept-source-agreements --accept-package-agreements
 winget install -e --id Postman.Postman --accept-source-agreements --accept-package-agreements
